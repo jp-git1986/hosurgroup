27159888d3ceac7f25eb7af1181a53290da4ab00
jp-git1986 <54586127+jp-git1986@users.noreply.github.com>
2021-06-29 17:30:02 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,19 @@
-def call(name) {
-node () {
-echo String.format("Hello ${name}")
-}
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+      agent any
+        stages {
+          stage ('stage1') {
+            steps {
+              sh 'echo pipelineParams.var1'
+              sh 'echo pipelineParams.var2'
+            }
+    }
+        }
+      }
 }
