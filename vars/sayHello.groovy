61adb8c35eeb76b270aa079089ae2f50fbae5a8a
jp-git1986 <54586127+jp-git1986@users.noreply.github.com>
2021-06-29 20:46:43 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,15 @@
+class jsparams implements Serializable {
+  //
+  static giveMeParameters (script) {
+    return [
+      // Some parms
+      script.string(defaultValue: 'f672054-1.x86_64', description: 'Enter app version', name: 'app_version'),
+      script.choice(choices: ['10.80.66.171','10.80.67.6','10.80.67.200'], description: 'Select a host to be delpoyed', name: 'host'),
+    ]
+  }
+}
+
+
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def pipelineParams= [:]
