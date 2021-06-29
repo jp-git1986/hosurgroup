9d4f5dc511d6087ecb7aee3b49a19333d4791142
jp-git1986 <54586127+jp-git1986@users.noreply.github.com>
2021-06-29 22:25:00 +0530

    Update params.groovy

--- vars/params.groovy
+++ vars/params.groovy
@@ -1,2 +1,10 @@
-var1 = 'jp'
-var2 = 'shiva'
+class jsParams implements Serializable {
+  //
+  static giveMeParameters (script) {
+    return [
+      // Some parms
+      script.string(defaultValue: 'f672054-1.x86_64', description: 'Enter app version', name: 'app_version'),
+      script.choice(choices: ['10.80.66.171','10.80.67.6','10.80.67.200'], description: 'Select a host to be delpoyed', name: 'host'),
+    ]
+  }
+}
