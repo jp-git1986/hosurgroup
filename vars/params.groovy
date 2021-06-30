adc5ec2efbfedcb0b7c46d06f3238014349fdfe1
jp-git1986 <54586127+jp-git1986@users.noreply.github.com>
2021-06-30 10:50:11 +0530

    Update params.groovy

--- vars/params.groovy
+++ vars/params.groovy
@@ -1,10 +1,17 @@
-class jsParams implements Serializable {
+//class jsParams implements Serializable {
   //
-  static giveMeParameters (script) {
-    return [
+ // static giveMeParameters (script) {
+ //   return [
       // Some parms
-      script.string(defaultValue: 'f672054-1.x86_64', description: 'Enter app version', name: 'app_version'),
-      script.choice(choices: ['10.80.66.171','10.80.67.6','10.80.67.200'], description: 'Select a host to be delpoyed', name: 'host'),
-    ]
-  }
-}
+ //     script.string(defaultValue: 'f672054-1.x86_64', description: 'Enter app version', name: 'app_version'),
+ //     script.choice(choices: ['10.80.66.171','10.80.67.6','10.80.67.200'], description: 'Select a host to be delpoyed', name: 'host'),
+ //   ]
+ // }
+//}
+
+properties( [
+    parameters([
+        choice(choices: ['10.80.66.171','10.80.67.6','10.80.67.200'], description: 'Select a host to be delpoyed', name: 'host')
+        ,string(defaultValue: 'f672054-1.x86_64', description: 'Enter app version', name: 'app_version')
+    ])
+] )
